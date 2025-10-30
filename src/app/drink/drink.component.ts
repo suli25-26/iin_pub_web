0794ef508815b89c8833bf56d0a64894861ee051
Erős István <eros@valami>
2025-10-30 11:12:04 +0100

    drink komponens create művelet ok

--- src/app/drink/drink.component.ts
+++ src/app/drink/drink.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
 import { DrinkapiService } from '../shared/drinkapi.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-drink',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './drink.component.html',
   styleUrl: './drink.component.css'
 })
 export class DrinkComponent {
 
+  drinkForm!: any;
   drinks: any;
+  addMode = true;
 
-  constructor(private api: DrinkapiService){}
+  constructor(
+    private api: DrinkapiService,
+    private build: FormBuilder
+  ){}
 
   ngOnInit() {
     this.getDrinks();
+    this.drinkForm = this.build.group({
+      id: [''],
+      drink: [''],
+      amount: [''],
+      price: [''],
+      type: [''],
+      package: ['']
+    });
+  }
+
+  save() {
+    console.log('Mentés...')
+    this.api.createDrink$(this.drinkForm.value).subscribe({
+      next: (result) => {
+        console.log(result);
+        this.drinkForm.reset();
+        this.getDrinks();
+      }
+    });
   }
 
   getDrinks() {
